c851b5b79e8860c81801cda79f2ba0841c9c1c27
Maxwell Heiber <mheiber@bloomberg.net>
2018-06-25 17:46:17 -0400

    add detail to 'no modifier with private name` case

--- tests/cases/conformance/classes/members/privateNames/privateNamesNoAccessibilityModifiers.ts
+++ tests/cases/conformance/classes/members/privateNames/privateNamesNoAccessibilityModifiers.ts
@@ -1,3 +1,25 @@
+/*
+This is different from the approach described in https://github.com/Microsoft/TypeScript/issues/16324#issuecomment-306890149.
+The reason for the difference is that we cannot currently see a use case for `private #foo`. `private #foo`
+is more restrictive than plain `#foo` because `private` prevents structural typing when using classes as types:
+
+```ts
+class A {
+   private foo;
+}
+
+class B extends A {}
+
+class C extends A {}
+
+let x: A;
+x = new B() // Error
+x = new C() // Error
+```
+
+
+
+*/
 class A {
     public #foo;                 // Error: Modifiers are not allowed for fields with private names
     private #foo;                // Error: Modifiers are not allowed for fields with private names
