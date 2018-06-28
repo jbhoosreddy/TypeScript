2389a52515c5b1b2d772ec01b7413dc67dc682e8
Maxwell Heiber <mheiber@bloomberg.net>
2018-06-27 22:11:47 -0400

    Update privateNameNotAllowedOutsideClassBody.ts

--- tests/cases/conformance/classes/members/privateNames/privateNameNotAllowedOutsideClassBody.ts
+++ tests/cases/conformance/classes/members/privateNames/privateNameNotAllowedOutsideClassBody.ts
@@ -1,5 +1,14 @@
 #foo = "foo";            // Error: Private names are not allowed outside of class bodies
 
 class A {
-    #bar = "bar";       // OK
+    #bar = "bar";        // OK
+    getString() {
+        return "string";
+    }
 }
+
+A.prototype.getString = function () {
+    // early error in ES spec: https://tc39.github.io/proposal-class-fields/#sec-static-semantics-early-errors
+    return this.#bar;    // Error: Private names are not allowed outside of class bodies
+};
+
